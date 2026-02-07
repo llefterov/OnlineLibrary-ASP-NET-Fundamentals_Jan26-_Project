b9e182ed71dd13174147e79318d736650548afff
llefterov <liubo_lefterov@yahoo.co.uk>
2026-02-07 18:56:15 +0200

    Add project files.

---
+++ OnlineLibrary/Data/OnlineLibraryDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineLibrary.Data
+{
+    public class OnlineLibraryDbContext(DbContextOptions<OnlineLibraryDbContext> options) : IdentityDbContext(options)
+    {
+
+
+    }
+}
