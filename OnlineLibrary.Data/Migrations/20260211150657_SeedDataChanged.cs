3db13be31cb88c6d2644889533aa2bd3f58c50fc
llefterov <liubo_lefterov@yahoo.co.uk>
2026-02-11 17:10:26 +0200

    ConfigurationFiles amended. DatBase updated.

---
+++ OnlineLibrary.Data/Migrations/20260211150657_SeedDataChanged.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineLibrary.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDataChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "FullName",
+                value: "R.R. Tolkien");
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "FullName",
+                value: "Agatha Christie");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "FullName",
+                value: "Agatha Christie");
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "FullName",
+                value: "J.K. Rowling");
+        }
+    }
+}
